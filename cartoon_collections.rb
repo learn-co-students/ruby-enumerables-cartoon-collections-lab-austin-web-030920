1802f20fc8aaafec8fa4399af836d53b005793c6
Dakota Willingham <kota@vaga.tk>
2020-02-27 21:51:32 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  counter = 0
+  array.each_with_index do |element, index|
+    puts "#{index + 1}. #{element}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  caps_calls = []
+  array.each do |e|
+    # caps = e.capitalize
+    caps_calls << "#{e.capitalize}!"
+  end
+  caps_calls
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any?{|e| e.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find { |word| cheese_types.include?(word) }
 end
